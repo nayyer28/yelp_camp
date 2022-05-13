7ed680b73cb4e525ce771b5c77946f9f6ac55ca6
saahil <nayyersaahil@gmail.com>
2022-05-13 20:51:14 +0200

    YelpCamp: sets up crud routes

---
+++ seeds/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('database connected!'));
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+	await Campground.deleteMany({});
+	const c = new Campground({ title: 'purple fields' });
+	for (let i = 0; i < 50; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		const camp = new Campground({
+			title: `${sample(descriptors)} ${sample(places)}`,
+			location: `${cities[random1000].city}, ${cities[random1000].state}`
+		});
+		await camp.save();
+	}
+};
+
+seedDB().then(() => {
+	db.close();
+});
